65f7b38de1442475559270ed8f4c19ec5cb694df
Henry Li <henry1943@163.com>
2017-05-21 18:39:55 +0800

    Styling

--- src/ocn/scn/BindTvScene.jsx
+++ src/ocn/scn/BindTvScene.jsx
@@ -13,9 +13,13 @@ export default class BindTvScene extends React.Component {
       needResult: 1,
       scanType: ['qrCode', 'barCode'],
       success(res) {
-
+        alert(res.resultStr);
       }
     });
+  }
+
+  handleNextButtonClick() {
+
   }
 
   render() {
@@ -24,13 +28,31 @@ export default class BindTvScene extends React.Component {
       <div className="ocn-bind-tv-scene ocn-scene">
         <div className="bottom">
           <div className="main">
-            <div className="thumbnail" style={{backgroundImage: `url(${user.headimgurl})`}} ></div>
-            <h3>终于等到你了，{user.nickname}</h3>
-            <div className="hint">打开机顶盒，在东方有线主界面中点击“我的二维码”</div>
-            <button className="primary button" onClick={() => this.handleBindButtonClick()}>扫码绑定我的机顶盒</button>
+            {this.renderMain(user)}
           </div>
         </div>
       </div>
     );
+  }
+
+  renderMain(user) {
+    if (user.ocnId) {
+      return (
+        <div className="main">
+          <div className="thumbnail" style={{ backgroundImage: `url(${user.headimgurl})` }} ></div>
+          <h3>终于等到你了，{user.nickname}</h3>
+          <div className="hint">打开机顶盒，在东方有线主界面中点击“我的二维码”</div>
+          <button className="primary button" onClick={() => this.handleBindButtonClick()}>扫码绑定我的机顶盒</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="main">
+          <div className="thumbnail" style={{ backgroundImage: `url(${user.headimgurl})` }} ></div>
+          <h3>太棒了，<br/>你已经可以在手机上选片了！</h3>
+          <button className="success button" onClick={() => this.handleNextButtonClick()}>下一步</button>
+        </div>
+      );
+    }
   }
 }
