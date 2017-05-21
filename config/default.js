37ebc93f40cc7085613dae47d769cf11cea01443
Henry Li <henry1943@163.com>
2017-05-21 18:26:18 +0800

    Edit url

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ module.exports = {
   wechat: {
     appId: 'wxcd1ad2cfb81d38ef',
     appSecret: '9b8ec7388c37dc02ff1d6d66587882af',
-    redirectUrl: 'http://ocn.daocloud.io/oauth/wechat/redirect'
+    redirectUrl: 'http://ocn.daoapp.io/oauth/wechat/redirect'
   },
   security: {
     allowAnonymous: ['/oauth/wechat/redirect']
